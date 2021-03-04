d3496813ce224f09362a40b50c66f2d9593a7e05
Zakhar <38567289+maZe0z@users.noreply.github.com>
2021-03-04 11:55:02 +0300

    Initial commit

---
+++ task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZDateTime zDateTime = new ZDateTime();
+            
+            zDateTime.ToFirst();
+            
+            zDateTime.ToSecond();
+            
+            Console.WriteLine(zDateTime.FirstFormat);
+            
+            Console.WriteLine(zDateTime.SecondFormat);
+            
+            zDateTime.CountDigits();
+        }
+    }
+}
---
+++ task3/ZDateTime.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace task3
+{
+    public class ZDateTime
+    {
+        public DateTime CurrentTime { get; set; } = DateTime.Now;
+
+        public string FirstFormat { get; set; }
+
+        public string SecondFormat { get; set; }
+
+        public void ToFirst()
+        {
+            FirstFormat = (CurrentTime.ToString("d",
+                CultureInfo.CreateSpecificCulture("en-NZ"))) + " " + (CurrentTime.ToString("t",
+                CultureInfo.CreateSpecificCulture("hr-HR")));
+            ;
+        }
+
+        public void ToSecond()
+        {
+            SecondFormat = (CurrentTime.ToString("d",
+                CultureInfo.CreateSpecificCulture("en-US"))) + " " + (CurrentTime.ToString("t",
+                CultureInfo.CreateSpecificCulture("en-US")));
+            ;
+        }
+
+        public void CountDigits()
+        {
+            int[] arr = new int[10];
+            int number = 0;
+            
+            foreach (char c in FirstFormat) {
+                int diff = c - '0';
+
+                if (diff < 0 || diff > 9) continue;
+                
+                arr[diff]++;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Amount of " + number + "'s" + " : " + arr[i]);
+                number++;
+            }
+        }
+    }
+}
